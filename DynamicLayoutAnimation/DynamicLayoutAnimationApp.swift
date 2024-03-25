d2bfbe333da2ba1a42cf1f3f25c77f400d2c18f8
Alisa <alisa.melissa@icloud.com>
2024-03-25 19:13:09 +0200

    Initial Commit

---
+++ DynamicLayoutAnimation/DynamicLayoutAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicLayoutAnimationApp.swift
+//  DynamicLayoutAnimation
+//
+//  Created by Алиса Третьякова on 25.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct DynamicLayoutAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
